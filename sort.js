ff885f87c1267e0144e95e4ee19f764b91c48754
Naman <namans714@gmail.com>
2026-01-16 17:41:34 +0530

    bubblesort

---
+++ sort.js
@@ -0,0 +1,17 @@
+function sort(arr){
+let n = arr.length;
+for (let i=0; i<n-1; i++) {
+        for (let j=0; j<n-i-1; j++) 
+        {
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp
+                       }
+        }
+        }
+        return arr;
+        }
+let arr = [4, 3, 5, 1, 2, 7, 9];
+sort(arr);
+console.log(arr);
